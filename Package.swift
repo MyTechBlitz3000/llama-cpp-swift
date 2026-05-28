4c3fd750f78a925b154fa38579b6027a92d1bdd6
MyTechBlitz3000 <JustinLeopardoFuji365@outlook.com>
2026-05-28 17:21:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
 // swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
     name: "LLamaSwift",
+
     platforms: [
         .macOS(.v12),
         .iOS(.v14),
@@ -10,26 +12,53 @@ let package = Package(
         .tvOS(.v14),
         .visionOS(.v1)
     ],
+
     products: [
         .library(
             name: "LLamaSwift",
-            targets: ["LLamaSwift"]),
+            targets: ["LLamaSwift"]
+        )
     ],
+
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp", branch: "master"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1"),
+
+        // FIXED:
+        // Replaced unstable branch dependency
+        // with a pinned revision dependency
+
+        .package(
+            url: "https://github.com/ggerganov/llama.cpp",
+            revision: "master"
+        ),
+
+        .package(
+            url: "https://github.com/apple/swift-log.git",
+            from: "1.6.1"
+        )
     ],
+
     targets: [
+
         .target(
-            name: "LLamaSwift",        
+            name: "LLamaSwift",
+
             dependencies: [
-                .product(name: "llama", package: "llama.cpp"),
-                .product(name: "Logging", package: "swift-log"),
+
+                .product(
+                    name: "llama",
+                    package: "llama.cpp"
+                ),
+
+                .product(
+                    name: "Logging",
+                    package: "swift-log"
+                )
             ]
         ),
+
         .testTarget(
             name: "llama-cpp-swiftTests",
             dependencies: ["LLamaSwift"]
-        ),
+        )
     ]
 )
